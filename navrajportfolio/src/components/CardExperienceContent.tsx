9c555dc0bf8ce01a9fbd56f839e189ab42bf9b56
Navraj singh kaler <navrajkaler996@gmail.com>
2023-05-03 19:28:15 -0500

    functionality completed

--- navrajportfolio/src/components/CardExperienceContent.tsx
+++ navrajportfolio/src/components/CardExperienceContent.tsx
@@ -1,17 +1,26 @@
 import {  useState } from "react"
-
+import ExperienceData from "../data/ExperienceData";
 
 const CardExperienceContent = () => {
 
-  const [style, setStyle] = useState("")
+  const [style, setStyle] = useState("");
+  const [selected, setSelected ] = useState<{company: String, designation: String, dates: String, city: String, country: String, description: String, details: Array<String>}>()
 
+  const handleClick = (value: string) => {
+    const selectedExperience = ExperienceData.find((d) => d.company == value )
+    setSelected(selectedExperience)
+    setStyle("card__experience-details card__experience-details--down")
+  }  
+
+  const handleClose = () => {
+    setStyle("");
+  }
 
   return (
     <div className="card__content--experience">
-
         <div className="card__vt-line">
           <div className="card__vt-line-circle">
-            <p className="first"> <span className="position"> Trainee </span><br/> <span className="company"> Infosys</span><br/><span className="date">Feb, 2019 - Aug, 2019 </span> <br/> <span className="know-more" onClick={()=> setStyle("card__experience-details card__experience-details--down")}>know more...</span></p>
+            <p className="first"> <span className="position"> Trainee </span><br/> <span className="company"> Infosys</span><br/><span className="date">Feb, 2019 - Aug, 2019 </span> <br/> <span className="know-more" onClick={()=> handleClick("Telus international")}>know more...</span></p>
           </div>
           <div className="card__vt-line-circle">
           <p className="second"><span className="position"> Systems Engineer</span> <br/><span className="company">Infosys</span><br/><span className="date">Aug 2019 - Aug, 2021 </span> <br/> <span className="know-more" onClick={()=> setStyle("card__experience-details card__experience-details--top")}>know more...</span></p>
@@ -21,34 +30,22 @@ const CardExperienceContent = () => {
           </div>
           <div className="card__vt-line-circle">
           <p className="forth"><span className="position"> Sr. App developer</span> <br/> <span className="company">Telus International</span><br/><span className="date">Apr 2022 - Dec, 2022 </span> <br/> <span className="know-more" onClick={()=> setStyle("card__experience-details card__experience-details--down")}>know more...</span></p>
-          </div>
-          
+          </div>   
         </div>
         <div className={style} style={style?.length ===0 ? {display: "none"} : {}}>
-
           <div>
-          <p>Senior Application developer</p>
-          <p> Telus Internationa, Noida, India</p>
-          <p>April 4, 2022 - Decemeber 23, 2022</p>
-          <p className="description">
-             I worked on a single react-based web application which was started from scratch. We used webpack and babel along with TypeScript to setup this project. Redux was used for state management while Sass was used for CSS. 
-           </p> 
+            <p id="check-next" onClick={() => handleClose()} >x</p>
+            <p>{selected?.designation}</p>
+            <p> {selected &&  `${selected?.company}, ${selected?.city}, ${selected?.country}`}</p>
+            <p>{selected?.dates}</p>
+            <p className="description">
+              {selected?.description}
+            </p> 
             <ul>
-              <li>Worked as a full-stack developer.</li>
-              <li>Used React.js along with Redux and Sass on frontend.</li>
-              <li>Used fastify and node.js on backend.</li>
-              <li>Worked in a team of 12; 9 developers and 3 testers.</li>
-              <li>Regularly communicated with the product owners.</li>
-              <li>Regularly contributed in the project planning.</li>
-              <li>Headed a sub-team of 4.</li>
-     
+              {selected?.details?.map((d,i) => <li key={i}>{d}</li>)}    
             </ul> 
           </div>
-         
-         
-
         </div>
-    
     </div>
   )
 }
---
+++ navrajportfolio/src/data/experienceData.tsx
@@ -0,0 +1,22 @@
+const experienceData = [
+  {
+    company: "Telus international",
+    designation: "Senior Application developer",
+    dates: "April 4, 2022 - Decemeber 23, 2022",
+    city: "Noida",
+    country: "India",
+    description:
+      "  I worked on a single react-based web application which was started from scratch. We used webpack and babel along with TypeScript to setup this project. Redux was used for state management while Sass was used for CSS. ",
+    details: [
+      "Worked as a full-stack developer.",
+      "Used React.js along with Redux and Sass on frontend.",
+      "Used fastify and node.js on backend.",
+      "Worked in a team of 12; 9 developers and 3 testers.",
+      "Regularly communicated with the product owners.",
+      "Regularly contributed in the project planning.",
+      "Headed a sub-team of 4.",
+    ],
+  },
+];
+
+export default experienceData
